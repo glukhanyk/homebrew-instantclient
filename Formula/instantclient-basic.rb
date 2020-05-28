e2797a9ac1c33bff080cc964cc326fb45e6f49dc
glukhanyk <glukhanyk@gmail.com>
2020-05-28 15:33:49 +0300

    10.2.0.4

--- Formula/instantclient-basic.rb
+++ Formula/instantclient-basic.rb
@@ -3,8 +3,8 @@ class InstantclientBasic < Formula
   desc "Oracle Instant Client Basic x64"
   homepage "https://www.oracle.com/database/technologies/instant-client/macos-intel-x86-downloads.html"
 
-  url "https://download.oracle.com/otn_software/mac/instantclient/193000/instantclient-basic-macos.x64-19.3.0.0.0dbru.zip"
-  sha256 "f4335c1d53e8188a3a8cdfb97494ff87c4d0f481309284cf086dc64080a60abd"
+  url "https://download.oracle.com/otn_software/mac/instantclient/10204/instantclient-basic-10.2.0.4.0-macosx-x64.zip"
+  sha256 "ae0fc5b2148f58d4f3ed0f3d10fc2add50be33d1ac2f360fadb5be8eca28a4a7"
 
   conflicts_with "instantclient-basiclite"
 
